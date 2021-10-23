7fe51f4647e7fd1d902ed9009faaac74382c5d41
Jaden-Reklaw <jwalker117@gmail.com>
2021-10-23 11:22:22 -0500

    s12 162

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -33,6 +33,16 @@ const Search = () => {
     const renderResults = results.map((result) => {
         return(
             <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a 
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`} 
+                        className="ui button"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Go
+                    </a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
